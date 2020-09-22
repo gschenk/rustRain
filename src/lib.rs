872c865f2f8793bdf4ed0d97f6c90d05b581988d
gerald schenk <gschenk@gmx.net>
2020-09-22 17:25:05 +0200

    provides test cases from fn, adds Problem test

--- src/lib.rs
+++ src/lib.rs
@@ -38,3 +38,108 @@ impl Problem {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[derive(Clone)]
+    struct Case(u64, Vec<u64>, Vec<f64>);
+
+    // simple test cases, pen-and-paper results
+    fn provide_cases(token: &str) -> Vec<Case> {
+        let simple = vec![
+            Case(
+                0,
+                vec![5, 5, 0, 0, 0, 0, 5, 5],
+                vec![5.0, 5.0, 3.0, 3.0, 3.0, 3.0, 5.0, 5.0],
+            ),
+            Case(
+                1,
+                vec![5, 5, 0, 0, 0, 0, 5, 5],
+                vec![5.0, 5.0, 3.0, 3.0, 3.0, 3.0, 5.0, 5.0],
+            ),
+            Case(
+                2,
+                vec![5, 5, 0, 0, 0, 0, 5, 5],
+                vec![5.0, 5.0, 4.0, 4.0, 4.0, 4.0, 5.0, 5.0],
+            ),
+        ];
+        let saturation = vec![
+            Case(1, vec![2, 0, 0, 2], vec![2.0, 2.0, 2.0, 2.0]),
+            Case(
+                2,
+                vec![4, 4, 0, 0, 0, 0, 4, 4],
+                vec![4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0],
+            ),
+        ];
+        let oversaturation = vec![Case(
+            3,
+            vec![5, 5, 0, 0, 0, 0, 5, 5],
+            vec![5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5],
+        )];
+        let symmetry = vec![
+            Case(
+                1,
+                vec![5, 4, 3, 0, 0, 0],
+                vec![5.0, 4.0, 3.0, 2.0, 2.0, 2.0],
+            ),
+            Case(
+                1,
+                vec![0, 0, 0, 4, 3, 5],
+                vec![2.0, 2.0, 2.0, 3.0, 4.0, 5.0],
+            ),
+            Case(2, vec![7, 6, 5, 0, 0], vec![7.0, 6.0, 5.5, 5.5, 5.5]),
+            Case(2, vec![0, 0, 5, 6, 7], vec![5.5, 5.5, 5.5, 6.0, 7.0]),
+        ];
+        let watersheds = vec![
+            Case(1, vec![0, 3, 0], vec![1.5, 3.0, 1.5]),
+            Case(1, vec![0, 3, 3, 0], vec![2.0, 3.0, 3.0, 2.0]),
+            Case(
+                1,
+                vec![6, 0, 4, 4, 0, 6],
+                vec![6.0, 3.0, 4.0, 4.0, 3.0, 6.0],
+            ),
+            Case(
+                2,
+                vec![0, 5, 0, 5, 0, 5, 0],
+                vec![3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0],
+            ),
+            Case(
+                2,
+                vec![0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0],
+                vec![
+                    3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0, 5.0, 3.0,
+                ],
+            ),
+            Case(1, vec![0, 3, 4, 3, 0], vec![2.5, 3.0, 3.0, 2.5]),
+        ];
+        if token == "saturation" {
+            return saturation;
+        }
+        if token == "all" {
+            return [
+                simple.as_slice(),
+                saturation.as_slice(),
+                oversaturation.as_slice(),
+                symmetry.as_slice(),
+                watersheds.as_slice(),
+            ]
+            .concat();
+        }
+        return vec![];
+    }
+
+    #[test]
+    fn saturation() {
+        let cases = provide_cases("saturation");
+        for case in cases.iter() {
+            let Case(a, b, _) = case;
+            let problem = Problem::new(*a, &b);
+
+            // expect that water saturation and total water are equal
+            assert_eq!(problem.saturation_water, problem.water_tot);
+        }
+    }
+}
