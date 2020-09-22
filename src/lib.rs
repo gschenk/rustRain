0a8576a04d5dbdebd7373d85f46c1f9b351d245d
gerald schenk <gschenk@gmx.net>
2020-09-22 21:11:55 +0200

    adds solution Struct and test

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,35 @@ impl Problem {
 pub mod solve {
     use super::{f64equal, Problem};
 
+    // Solution stores results
+    // levels are the overal levels of water or dry land per segment,
+    // which ever is on top
+    // water covers is only the amount of water upon the the land
+    // water_tot is the overal amount of water, for plausibility checks
+    pub struct Solution {
+        pub levels: Vec<f64>,
+        pub water_covers: Vec<f64>,
+        pub water_tot: f64,
+    }
+
+    impl Solution {
+        // arguments: levels: a vector of ground/water levels
+        // grounds: slice of bare grounds
+        pub fn new(levels: Vec<f64>, grounds: &[f64]) -> Solution {
+            let water_covers: Vec<f64> = levels
+                .iter()
+                .zip(grounds.iter())
+                .map(|(&a, &b)| a - b)
+                .collect();
+            let water_tot = water_covers.iter().sum();
+            Solution {
+                levels,
+                water_covers,
+                water_tot,
+            }
+        }
+    }
+
     // categorise problems to deal with trivial and simple problems
     // returns strings as placeholder. Will return function.
     pub fn categorise(problem: Problem) -> &'static str {
@@ -195,5 +224,16 @@ mod tests {
         assert_eq!(f64equal(a, b), true);
         assert_ne!(f64equal(a, c), true);
         assert_ne!(f64equal(a, d), true);
+    }
+
+    #[test]
+    fn solutions() {
+        let a_vec: Vec<f64> = vec![3.0, 2.0, 2.0];
+        let bs: Vec<f64> = vec![3.0, 1.0, 0.0];
+        let expected_1: Vec<f64> = vec![0.0, 1.0, 2.0];
+        let expected_2: f64 = 3.0;
+        let received = solve::Solution::new(a_vec, &bs);
+        assert_eq!(expected_1, received.water_covers);
+        assert_eq!(expected_2, received.water_tot);
     }
 }
